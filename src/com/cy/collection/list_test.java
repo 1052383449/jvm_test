6c9b82a80224f938fd429f05bc89b89c5d7529eb
尹双玉 <ysy@bsoft.com.cn>
2021-05-31 13:39:08 +0800

    test

--- src/com/cy/collection/list_test.java
+++ src/com/cy/collection/list_test.java
@@ -1,9 +1,10 @@
 package com.cy.collection;
 
+import java.io.*;
 import java.util.*;
 
 public class list_test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         List<Integer> arrayList = new ArrayList<>();
         //初始容量测试,jdk1.8以前为10,1.8以后为0
         //添加元素时进行扩容，默认为10
@@ -18,19 +19,33 @@ public class list_test {
         ArrayList<Map<String,String>> mapList = new ArrayList<>(5);
         Map<String,String> map = new HashMap<>();
         map.put("ss","33");
+        Map<String,String> map2 = new HashMap<>();
+        map.put("ss2","332");
         mapList.add(map);
+        mapList.add(map2);
 
-        ArrayList<Map<String,String>> mapList1 = (ArrayList<Map<String,String>>)mapList.clone();
+        ArrayList<Map<String,String>> mapList1 = new ArrayList<>();
         Map<String,String>[] maps = new Map[1];
         Map<String,String>[] mapList2 =  mapList.toArray(maps);
 
-        map.put("ss","22");
+       /* map.put("ss","22");
         System.out.println(mapList1.get(0).toString());
-        System.out.println(mapList2[0].toString());
+        System.out.println(mapList2[0].toString());*/
 
-        //remove和set方法测试
+        //remove和set方法测试,都是通过System.arraycopy()来移动元素，移除掉的元素，置为空
         arrayList.remove(10);
+        mapList1.add(map);
+        mapList.removeAll(mapList1);
 
+        //序列化
+        ObjectOutputStream oos= new ObjectOutputStream(new FileOutputStream("D://out.text"));
+        oos.writeObject(mapList);
+
+        //反序列化
+        File file = new File("D://out.text");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        ArrayList<Map<String,String>> arrayList2 = (ArrayList<Map<String,String>>)ois.readObject();
+        System.out.println(arrayList2.toString());
         List<Integer> linkedList = new LinkedList<>();
 
 
